d0e68b64e23e251fc2a238ae7d15fee0701217e0
unknown <matech99@outlook.com>
2024-02-07 10:28:00 +0000

    Added sample design and CSS stylesheet

--- init.lua
+++ init.lua
@@ -1,61 +1,82 @@
+-- Module object
 muteSliders = {}
 
+-- Control components array:
+--sliderSystem = {
+--  {fader},{toggle}, {valueAtMute}
+--};
 sliderSystem = {}
--- Function to add a slider-toggle pair to the system
-function muteSliders.addSlider(slider, toggle)
-  if sliderSystem == nil then sliderSystem = {} end
-  -- Initialize slider_position_at_mute for each pair
-  local slider_position_at_mute = nil
-  local newRow = {slider, toggle, slider_position_at_mute}
-  table.insert(sliderSystem, newRow)
+
+-- Call this function to add a slider, passing it a fader and a toggle.
+function muteSliders.addSlider(fader, toggle)
+    if sliderSystem == nil then sliderSystem = {} end
+    -- Initialize slider_position_at_mute for each pair, 
+    -- this allows unmuting by moving the muted slider
+    local slider_position_at_mute = nil
+    local newRow = {fader, toggle, slider_position_at_mute}
+    -- Populate the array 
+    table.insert(sliderSystem, newRow)
+
+    muteSliders.assignEventHandlers()
+    -- Initial call to adjust styles based on values at load time 
+  muteSliders.callEventHandlers()
 end
 
-function muteSliders.Start()
--- Iterating over each slider-toggle pair
-for controlPair = 1, #sliderSystem do
-  -- Slider EventHandler
-  sliderSystem[controlPair][1].EventHandler = function()
-      -- Mute if slider positioned at 0
-      if sliderSystem[controlPair][1].Position == 0 then
-        muteSliders.sliderMute(controlPair)
-      end
-
-      -- Unmute if muted slider is moved
-      local slider_position_at_mute = sliderSystem[controlPair][3]
-      if sliderSystem[controlPair][1].Position ~= slider_position_at_mute then
-        muteSliders.sliderUnmute(controlPair)
-      end
-  end
-
-  -- Toggle EventHandler
-  sliderSystem[controlPair][2].EventHandler = function()
-      -- Muting and unmuting using toggle
-      if sliderSystem[controlPair][2].Boolean == true then
-          muteSliders.sliderMute(controlPair)
-      elseif sliderSystem[controlPair][2].Boolean == false then
-        muteSliders.sliderUnmute(controlPair)
-      end
-  end
-end
+-- Call the fader and toggle eventhandlers in sliderSystem array
+function muteSliders.callEventHandlers()
+  for controlPair = 1, #sliderSystem do
+    sliderSystem[controlPair][1].EventHandler()
+    sliderSystem[controlPair][2].EventHandler()
+    end
 end
 
-function muteSliders.sliderUnmute(controlPair)
-  if sliderSystem[controlPair][1].Position == 0 then sliderSystem[controlPair][1].Position = 0.1 end
-  sliderSystem[controlPair][2].Value = 0
-  -- Apply unmuted fader style
-  sliderSystem[controlPair][1].CssClass = "fader"
-  -- Reset the stored position
-  sliderSystem[controlPair][3] = nil
+function muteSliders.assignEventHandlers()
+    -- Iterating over each row in array
+    for controlPair = 1, #sliderSystem do
+        -- Slider EventHandler
+        sliderSystem[controlPair][1].EventHandler = function()
+            -- Mute if slider positioned at 0
+            if sliderSystem[controlPair][1].Position == 0 then
+                muteSliders.mute(controlPair)
+            end
+            -- Unmute if muted slider is moved
+            local slider_position_at_mute = sliderSystem[controlPair][3]
+            if sliderSystem[controlPair][1].Position ~= slider_position_at_mute then
+                muteSliders.unmute(controlPair)
+            end
+        end
+
+        -- Toggle EventHandler
+        sliderSystem[controlPair][2].EventHandler = function()
+            -- Muting and unmuting using toggle
+            if sliderSystem[controlPair][2].Boolean == true then
+                muteSliders.mute(controlPair)
+            elseif sliderSystem[controlPair][2].Boolean == false then
+                muteSliders.unmute(controlPair)
+            end
+        end
+    end
 end
 
-function muteSliders.sliderMute(controlPair)
-  -- Mute the toggle
-  sliderSystem[controlPair][2].Boolean = true
-  -- Save slider position
-  sliderSystem[controlPair][3] = sliderSystem[controlPair][1].Position
-  -- Apply muted fader style
-  sliderSystem[controlPair][1].CssClass = "fader_disabled"
+function muteSliders.unmute(controlPair)
+    if sliderSystem[controlPair][1].Position == 0 then sliderSystem[controlPair][1].Position = 0.1 end
+    sliderSystem[controlPair][2].Value = 0
+    -- Apply unmuted fader style
+    sliderSystem[controlPair][1].CssClass = "fader_unmuted"
+    sliderSystem[controlPair][2].CssClass = "muteToggle"
+    -- Reset the stored position
+    sliderSystem[controlPair][3] = nil
 end
 
+function muteSliders.mute(controlPair)
+    -- Mute the toggle
+    sliderSystem[controlPair][2].Boolean = true
+    -- Save slider position
+    sliderSystem[controlPair][3] = sliderSystem[controlPair][1].Position
+    -- Apply muted fader style
+    sliderSystem[controlPair][1].CssClass = "fader_muted"
+    sliderSystem[controlPair][2].CssClass = "muteToggle"
+
+end
 
 return muteSliders
